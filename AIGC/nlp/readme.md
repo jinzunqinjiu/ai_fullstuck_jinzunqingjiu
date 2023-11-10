be7f34fb0e355196da2525a329ea5381d728ace9
jinzunqinjiu <2422671867@qq.com>
2023-11-10 21:21:11 +0800

    AIGC nlp

---
+++ AIGC/nlp/readme.md
@@ -0,0 +1,12 @@
+# AIGC 学习方向
+
+- openai
+强大的api和LLM能力，AI应用开发的基础
+prompt engineer 提示词工程师
+- transformers
+    ai 的底层实现   细节
+    情感分析  pipeline('task',model)
+- langchain
+    AI的开发框架
+
+- AI助理帮助我们跨越开发能力边界，Facebook
---
+++ BATJTMD/baidu/1.js
@@ -0,0 +1,34 @@
+//es6的class类
+// 类跟对象有什么区别?
+// 类是抽象的，定制，
+// 对象是具体的
+class SingleDog{
+    constructor(){
+
+    }
+    static instance = null //静态属性
+    //属于类上的方法
+    static   getInstance(){
+
+        // console.log('类的方法')
+        if(!SingleDog.instance)
+        {
+            SingleDog.instance = new SingleDog()
+        }
+        return SingleDog.instance
+
+    }
+    //公有方法 属于实例上的
+    show(){
+        console.log('单身贵族')
+    }
+}
+// 设计模式 一个类只实例化
+// 第一次new,不实例化，直接返回实例
+// new角度搞不定？ & { }
+// 总领导   弹窗
+const s1 = SingleDog.getInstance()//生成对象->拿到对象
+const s2 = SingleDog.getInstance()
+// //如何让它们相等
+console.log(s1==s2)//False内存不一样
+// console.log( SingleDog.getInstance())
